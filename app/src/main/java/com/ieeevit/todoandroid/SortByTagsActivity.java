f1d15dd56ff561d3b48a4ca11e7b3e7953a8bd69
Fajar Septian <fajarajay10@gmail.com>
2020-09-30 22:05:08 +0700

    create new activity for SortByTagsActivity

---
+++ app/src/main/java/com/ieeevit/todoandroid/SortByTagsActivity.java
@@ -0,0 +1,14 @@
+package com.ieeevit.todoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SortByTagsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sort_by_tags);
+    }
+}
